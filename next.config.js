17102909f49ec5581fa2cf6512a5f75ad925c62e
Michael Mirandi <mmirandi@vt.edu>
2025-02-06 17:01:53 -0600

    :add mermaid support and intro - management structures

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 import nextra from 'nextra'
+import { remarkMermaid } from '@theguild/remark-mermaid'
 
 const withNextra = nextra({
-
+  mdxOptions: {
+    remarkPlugins: [remarkMermaid]
+  }
 })
 
+// Nextra wraps Next.js config
 export default withNextra({
   output: 'export', // Optional: for static exports
   images: {
