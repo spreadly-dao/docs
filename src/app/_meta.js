390fb2dbc46068c7a9d9f8a65fcbac3cc6793ffe
Michael Mirandi <mmirandi@vt.edu>
2025-02-07 01:42:14 -0600

    :add docs v1

---
+++ src/app/_meta.js
@@ -0,0 +1,8 @@
+export default {
+  "index": "Introduction",
+  "management-structures": "Management Structures",
+  "capital-formation-process": "Capital Formation",
+  "treasury-operations": "Treasury Operations", 
+  "staking-and-governance": "Staking and Governance",
+  "revenue-distribution": "Revenue Distribution"
+}
